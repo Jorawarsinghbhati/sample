657a514000d784d8a4d6f67cc411d38a5470ce4c
jorawar singh <2023ucp1619@mnit.ac.in>
2024-11-14 12:22:18 +0530

    change in q4

--- q4.py
+++ q4.py
@@ -7,6 +7,7 @@ x3=int(input("enter x3"))
 y1=int(input("enter y1"))
 y2=int(input("enter y2"))
 y3=int(input("enter y3"))
+y6=int(input("enter y6'))
 print(x1*y1+x2*y2+x3*y3)
 print("hello")
 <<<<<<< HEAD
